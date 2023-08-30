42d02cf1310aa0f358eff37b0b952f16c4c80d11
John Legg <johnllegg@gmail.com>
2023-08-30 14:00:10 -0400

    removing slack

--- projects/github-actions/repo-gardening/src/tasks/triage-issues/index.js
+++ projects/github-actions/repo-gardening/src/tasks/triage-issues/index.js
@@ -1,7 +1,6 @@
 const { getInput, setFailed } = require( '@actions/core' );
 const debug = require( '../../utils/debug' );
 const getLabels = require( '../../utils/get-labels' );
-const sendSlackMessage = require( '../../utils/send-slack-message' );
 
 /* global GitHub, WebhookPayloadIssue */
 
@@ -163,46 +162,6 @@ function findPriority( body ) {
 }
 
 /**
- * Build an object containing the slack message and its formatting to send to Slack.
- *
- * @param {WebhookPayloadIssue} payload - Issue event payload.
- * @param {string}              channel - Slack channel ID.
- * @param {string}              message - Basic message (without the formatting).
- * @returns {object} Object containing the slack message and its formatting.
- */
-function formatSlackMessage( payload, channel, message ) {
-	const { issue } = payload;
-	const { html_url, title } = issue;
-
-	return {
-		channel,
-		blocks: [
-			{
-				type: 'section',
-				text: {
-					type: 'mrkdwn',
-					text: message,
-				},
-			},
-			{
-				type: 'divider',
-			},
-			{
-				type: 'section',
-				text: {
-					type: 'mrkdwn',
-					text: `<${ html_url }|${ title }>`,
-				},
-			},
-		],
-		text: `${ message } -- <${ html_url }|${ title }>`, // Fallback text for display in notifications.
-		mrkdwn: true, // Formatting of the fallback text.
-		unfurl_links: false,
-		unfurl_media: false,
-	};
-}
-
-/**
  * Automatically add labels to issues, and send Slack notifications.
  *
  * This task can send 2 different types of Slack notifications:
@@ -217,18 +176,6 @@ async function triageIssues( payload, octokit ) {
 	const { number, body, state } = issue;
 	const { owner, name, full_name } = repository;
 	const ownerLogin = owner.login;
-
-	const slackToken = getInput( 'slack_token' );
-	if ( ! slackToken ) {
-		setFailed( 'triage-issues: Input slack_token is required but missing. Aborting.' );
-		return;
-	}
-
-	const channel = getInput( 'slack_quality_channel' );
-	if ( ! channel ) {
-		setFailed( 'triage-issues: Input slack_quality_channel is required but missing. Aborting.' );
-		return;
-	}
 
 	// Find Priority.
 	const priorityLabels = await hasPriorityLabels(
@@ -297,60 +244,7 @@ async function triageIssues( payload, octokit ) {
 				issue_number: number,
 				labels: [ `[Pri] ${ priority }` ],
 			} );
-
-			// If we're adding a TBD priority, if we're in the Calypso repo,
-			// send a Slack notification.
-			if ( priority === 'TBD' && full_name === 'Automattic/wp-calypso' ) {
-				debug(
-					`triage-issues: #${ number } doesn't have a Priority set. Sending in Slack message to the Kitkat team.`
-				);
-				const message = '@kitkat-team New bug missing priority. Please do a priority assessment.';
-				const slackMessageFormat = formatSlackMessage( payload, channel, message );
-				await sendSlackMessage( message, channel, slackToken, payload, slackMessageFormat );
-			}
 		}
-	}
-
-	/*
-	 * Send a Slack Notification if the issue is important.
-	 *
-	 * We define an important issue when meeting all of the following criteria:
-	 * - A bug (includes a "[Type] Bug" label, or a "[Type] Bug" label is added to the issue right now)
-	 * - The issue is still opened
-	 * - The issue is not escalated yet (no "[Status] Escalated" label)
-	 * - The issue is either a high priority or a blocker (inferred from the existing labels or from the issue body)
-	 * - The issue is not already set to another priority label (no "[Pri] High", "[Pri] BLOCKER", or "[Pri] TBD" label)
-	 */
-
-	const isEscalated = await hasEscalatedLabel( octokit, ownerLogin, name, number, action, label );
-
-	const highPriorityIssue = priority === 'High' || priorityLabels.includes( '[Pri] High' );
-	const blockerIssue = priority === 'BLOCKER' || priorityLabels.includes( '[Pri] BLOCKER' );
-
-	const hasOtherPriorityLabels = priorityLabels.some( priLabel =>
-		/^\[Pri\] (?!High|BLOCKER|TBD)/.test( priLabel )
-	);
-
-	if (
-		isBugIssue &&
-		state === 'open' &&
-		! isEscalated &&
-		( highPriorityIssue || blockerIssue ) &&
-		! hasOtherPriorityLabels
-	) {
-		const message = `New ${
-			highPriorityIssue ? 'High-priority' : 'Blocker'
-		} bug! Please check the priority.`;
-		const slackMessageFormat = formatSlackMessage( payload, channel, message );
-		await sendSlackMessage( message, channel, slackToken, payload, slackMessageFormat );
-
-		debug( `triage-issues: Adding a label to issue #${ number } to show that Kitkat was warned.` );
-		await octokit.rest.issues.addLabels( {
-			owner: ownerLogin,
-			repo: name,
-			issue_number: number,
-			labels: [ '[Status] Escalated' ],
-		} );
 	}
 }
 module.exports = triageIssues;
